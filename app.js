b596132363375ea2bc803ae223c36eca8319f399
Tomc17 <tommiecarey17@gmail.com>
2023-05-02 05:26:16 +0100

    Update app.js to use the ResponsiveVoice api

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-// create AudioContext object
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
 // get all anchor elements with the data-char attribute
 const buttons = document.querySelectorAll("a[data-char]");
 
 // get the logo element
 const logo = document.querySelector(".logo");
 
-// check if the browser supports speech synthesis
-const speechSynthesisSupported = "speechSynthesis" in window;
 const emoji = "\ud83d\udc04";
 // keep track of the last clicked button
 let lastButton = null;
@@ -21,18 +16,8 @@ let triggered = false;
 
 // add event listener to logo element
 logo.addEventListener("click", () => {
-  if (speechSynthesisSupported) {
-    speechSynthesis.cancel();
-    // create a new SpeechSynthesisUtterance object
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.lang = "ja-JP";
-    utterance.volume = 0.8;
-    utterance.rate = 0.9;
-    utterance.pitch = 1.5;
-    utterance.text = triggered ? "Moohidoi" : "Mouhidoi";
-    // speak the utterance
-    speechSynthesis.speak(utterance);
-  }
+  const text = triggered ? "Moohidoi" : "Mouhidoi";
+  responsiveVoice.speak(text, "Japanese Female", { rate: 0.9, pitch: 1.5, volume: 1 });
 });
 
 // loop through each button and add an event listener
@@ -40,39 +25,27 @@ buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     event.preventDefault();
 
-    if (speechSynthesisSupported) {
-      speechSynthesis.cancel();
-      // create a new SpeechSynthesisUtterance object
-      const utterance = new SpeechSynthesisUtterance();
-      utterance.lang = "ja-JP";
-      utterance.volume = 1;
-      utterance.rate = 0.75;
-      utterance.pitch = 1.5;
-      utterance.text = button.textContent;
-      // modify the utterance if the cow emoji is clicked
-if (button.textContent.includes(emoji)) {
-  utterance.text = "moooooo";
-} else {
-  utterance.text = button.textContent.replace(" ", "");
-}
-
-      // slow down the speech if the same button was clicked twice in a row
-      if (lastButton && lastButton.dataset.char === button.dataset.char) {
-        if (triggered) {
-          utterance.rate = 1;
-          triggered = false;
-        } else {
-          utterance.rate = 0.5;
-          triggered = true;
-        }
-      }
-
-      // speak the pronunciation
-      speechSynthesis.speak(utterance);
-
-      // update the last clicked button
-      lastButton = button;
+    let text = button.textContent;
+    if (button.textContent.includes(emoji)) {
+      text = "moooooo";
+    } else {
+      text = button.textContent.replace(" ", "");
     }
+
+    let rate = 0.75;
+    if (lastButton && lastButton.dataset.char === button.dataset.char) {
+      if (triggered) {
+        rate = 1;
+        triggered = false;
+      } else {
+        rate = 0.5;
+        triggered = true;
+      }
+    }
+
+    responsiveVoice.speak(text, "Japanese Female", { rate: rate, pitch: 1.5, volume: 10 });
+    // update the last clicked button
+    lastButton = button;
 
     // check for easter egg
     if (button.dataset.char === "mu") {
@@ -85,9 +58,11 @@ if (button.textContent.includes(emoji)) {
           element.textContent = "Moohidoi";
         });
         logo.innerHTML = "\ud83d\udc04";
-                  logo.style.fontSize = "4rem";
+        logo.style.fontSize = "4rem";
 
-document.querySelector('link[rel="icon"]').setAttribute("href", "images/moojito-icon.png");
+        document
+          .querySelector('link[rel="icon"]')
+          .setAttribute("href", "images/moojito-icon.png");
         document.title = "Moohidoi";
       }
     }
